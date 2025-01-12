60029162fbddd7b936367dde541c57d24a4de9c3
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-11 23:24:01 -0500

    page logic moved to cs file. Methods decomposed

--- Pages/Tasks/Delete.cshtml.cs
+++ Pages/Tasks/Delete.cshtml.cs
@@ -1,24 +1,57 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
+using WebApp.Data;
 
 namespace WebApp.Pages.Tasks
 {
     public class Delete : PageModel
     {
-        private readonly ILogger<Delete> _logger;
+        // private readonly ILogger<Delete> _logger;
 
-        public Delete(ILogger<Delete> logger)
+        // public Delete(ILogger<Delete> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        StringBuilder errorMessage = new StringBuilder();
+        ProgramContext context = new ProgramContext();
+
+        public async Task OnGet()
         {
-            _logger = logger;
+            int.TryParse(Request.Query["id"], out int id);
+
+            await DeleteTask(id);
+
+            Response.Redirect("/Tasks/Index");
         }
 
-        public void OnGet()
+        private async Task DeleteTask(int id)
         {
+            try
+            {
+                var task = context.Tasks.FirstOrDefault(t => t.Id == id);
+
+                context.Tasks.Remove(task);
+                await context.SaveChangesAsync();
+            }
+            catch (SqlException ex)
+            {
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessage.Append("Index #" + i + "\n" +
+                    "Message: " + ex.Errors[i].Message + "\n" +
+                    "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                    "Source: " + ex.Errors[i].Source + "\n" +
+                    "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+            }
         }
     }
 }
